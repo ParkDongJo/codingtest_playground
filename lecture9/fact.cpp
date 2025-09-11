cbd573e794b44012951f28a06ba96e929b9ca5e3
ParkDongJo <pdj10211@gmail.com>
2025-09-11 09:23:13 +0900

    feat: [A-9, A-10] 강의 듣고 개념 정리

--- lecture9/fact.cpp
+++ lecture9/fact.cpp
@@ -1,24 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int fact(int n) {
-    if (n == 0) return 1;
+int fact(int n)
+{
+    if (n == 0)
+        return 1;
     return n * fact(n - 1);
 }
-int fact1(int n) {
+
+// 재귀는 반복문으로 호출 가능
+int factByFor(int n)
+{
     int res = 1;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++)
+    {
         res *= i;
     }
     return res;
 }
-int fibo(int n) {
-    if (n <= 1) return n;
+int fibo(int n)
+{
+    if (n <= 1)
+        return n;
     return fibo(n - 1) + fibo(n - 2);
 }
 int n = 5;
 
-int main() {
+int main()
+{
     cout << fact(n) << "\n";
     cout << fibo(n) << "\n";
     return 0;
