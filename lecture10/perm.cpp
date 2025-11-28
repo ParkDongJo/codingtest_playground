bb368c5ca1f9428e8b0d5c267c846618c109d3fe
ParkDongJo <pdj10211@gmail.com>
2025-11-28 23:57:49 +0900

    feat : 순열 개념 정리

--- lecture10/perm.cpp
+++ lecture10/perm.cpp
@@ -24,17 +24,19 @@ using namespace std;
 //     return 0;
 // }
 
-
 // 만약 이중에 2개만 뽑는 순열을 구하고 싶다면?
-int main() {
+int main()
+{
     // vector<int> v = {1, 2, 3, 4, 5};
-    int a [] = {1, 3, 2, 4, 5};
+    int a[] = {1, 3, 2, 4, 5};
 
     // next_permutation() 을 사용하기 위해, 정렬이 되어 있어야 한다.
     // next_permutation() 오름차순 기반으로 순열을 생성한다.
     sort(&a[0], &a[0] + 5);
-    do {
-        for (int i = 0; i < 2; i++) {
+    do
+    {
+        for (int i = 0; i < 2; i++)
+        {
             cout << a[i] << " ";
         }
         cout << "\n";
